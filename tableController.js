d5e250a17f1a74306353a71e27c79a9d9c587c73
The man <ThisIsntTheWay@users.noreply.github.com>
2021-10-22 13:09:10 +0200

    Implement table data exfiltration

--- tableController.js
+++ tableController.js
@@ -59,3 +59,42 @@ function removeRow(oButton) {
 	var empTab = document.getElementById('partitionTable');
 	empTab.deleteRow(oButton.parentNode.parentNode.rowIndex); // buttton -> td -> tr
 }
+
+function getData() {
+	var t = document.getElementById("partitionTable");
+	var dataArray = new Array();
+	var skippedRows = 0;
+	
+	for (var r = 0, n = t.rows.length - 1; r < n; r++) {
+		var cellLength = t.rows[r].cells.length;
+		console.log("Row: " + r + ", Cell length: " + cellLength);
+		
+		// The tables for some reason generate empty rows.
+		// The code with skippedRows will compensate for those:
+		// Such rows are skipped, and to ensure proper dataArray index continuation, the next index will be
+		//  substracted by the amount of skipped rows
+		var index = r - skippedRows;
+		
+		if (cellLength > 0) {
+			dataArray[index] = new Array();
+			
+			for (var c = 1, m = t.rows[r].cells.length; c < m; c++) {
+				var data = "";
+				
+				if (r == 0) {
+					data = t.rows[r].cells[c].innerText;
+				} else {
+					data = t.rows[r].cells[c].getElementsByClassName("tableTextBox")[0].value;
+				}
+				
+				console.log("> Column " + c + " data: " + data);
+				
+				dataArray[index].push(data);
+			}
+		} else {
+			skippedRows++;
+		}
+	}
+	
+	return dataArray;
+}
