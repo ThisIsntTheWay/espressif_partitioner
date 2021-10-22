c473ab787f31f82d78ec5ac575a239111c7a42ee
The man <ThisIsntTheWay@users.noreply.github.com>
2021-10-22 18:04:25 +0200

    Add bytes size and fix hexadecimal conversion

--- manipulator.js
+++ manipulator.js
@@ -9,37 +9,46 @@ function updateSizeValues(referenceElement) {
 	var sizeUnitDOM = document.getElementById("sizeUnit");
 	var sizeHexDOM = document.getElementById("sizeHex");
 	
+	var totalSizeBDOM = document.getElementById("totalSizeB");
 	var totalSizeKbDOM = document.getElementById("totalSizeKb");
 	var totalSizeMbDOM = document.getElementById("totalSizeMb");
 	
+	console.log(referenceElement);
+	
+	if (referenceElement.id != "sizeHex") {
+		console.log("WOWEE");
+	} else {
+		rawSize = parseInt(sizeHexDOM.value, 16);
+	}
 	
 	// Determine proper source for rawSize calculation
-	switch (referenceElement.id) {
-		case 'sizeHex':
-			break;
-		default:
-			switch (sizeUnitDOM.value) {
-				case 'b': rawSize = sizeFieldDOM.value; break;
-				case 'kb': rawSize = sizeFieldDOM.value / 1000; break;
-				case 'mb': rawSize = sizeFieldDOM.value / 1000000; break;
-			}
-			break;
+	switch (sizeUnitDOM.value) {
+		case 'b': rawSize = sizeFieldDOM.value; break;
+		case 'kb': rawSize = sizeFieldDOM.value / 1000; break;
+		case 'mb': rawSize = sizeFieldDOM.value / 1000000; break;
 	}
+	
 
 	switch (sizeUnitDOM.value) {
-		case 'b': 
+		case 'b':
+			totalSizeBDOM.innerHTML = rawSize;
 			totalSizeKbDOM.innerHTML = rawSize / 1000;
 			totalSizeMbDOM.innerHTML = rawSize / 1000000;
 			break;			
-		case 'kb': 
+		case 'kb':
+			totalSizeBDOM.innerHTML = rawSize * 1000000;
 			totalSizeKbDOM.innerHTML = rawSize * 1000;
 			totalSizeMbDOM.innerHTML = rawSize;
 			break;
 		case 'mb': 
+			totalSizeBDOM.innerHTML = rawSize * 1000000000000;
 			totalSizeKbDOM.innerHTML = rawSize * 1000000000;
 			totalSizeMbDOM.innerHTML = rawSize * 1000000;
 			break;
 	}
+	
+	sizeHexDOM.value = parseInt(totalSizeBDOM.innerHTML).toString(16);
+	
 }
 
 function toggleOverlay() {
